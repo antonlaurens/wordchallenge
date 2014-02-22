ae310100befc6894ffdb8edd2d4ba29ca89c771a
Anton Laurens <antonlaurens@gmail.com>
2014-02-22 18:16:25 +0000

    swap double for single quotes

--- js/lists.js
+++ js/lists.js
@@ -1,88 +1,82 @@
 var WordChallenge = window.WordChallenge || {};
 (function(S) {
 
-    var lists = [
+    var ListCollections = [
         {
-            id: 0,
-            name: 'List 1',
-            items: [
-                    "A boy's name",
-                    "Toys",
-                    "An animal",
-                    "Things that are cold",
-                    "Insects",
-                    "TV Shows",
-                    "Things that grow",
-                    "Fruits",
-                    "Things that are black",
-                    "School subjects",
-                    "Movie titles",
-                    "Musical Instruments"
-            ]
-        },
-        {
-            id: 1,
-            name: 'List 2',
-            items: [
-                "Authors","Bodies of water","A bird","Countries","Cartoon characters","Holidays","Things that are square","European city","Clothing","A relative","Games","Sports Stars"
-            ]
-        },
-        {
-            id: 2,
-            name: 'List 3',
-            items: [
-                "School supplies","Things that are hot","Heroes","A girl's name","Fears","TV Stars","Colors","A fish","Fruits","Provinces or States","Sports equipment","Tools"
-            ]
-        },
-        {
-            id: 3,
-            name: 'List 4',
-            items: [
-                "Breakfast foods","Gifts","Flowers","Ice cream flavors","A drink","A river","Cities","Things in the kitchen","Ocean things","Nicknames","Hobbies","Parts of the body"
-            ]
-        },
-        {
-            id: 4,
-            name: 'List 5',
-            items: [
-                "Sandwiches","Items in a catalog","World leaders/ Politicians","School subjects","Excuses for being late","Ice cream flavors","Things that jump/bounce","Television stars","Things in a park","Foreign cities","Stones/Gems","Musical instruments"
-            ]
-        },
-        {
-            id: 5,
-            name: 'List 6',
-            items: [
-                "Nicknames","Things in the sky","Pizza toppings","Colleges/Universities","Fish","Countries","Things that have spots","Historical Figures","Something you're afraid of","Terms of endearment","Items in this room","Drugs that are abused"
-            ]
-        },
-        {
-            id: 6,
-            name: 'List 7',
-            items: [
-                "Fictional characters","Menu items","Magazines","Capitals","Kinds of candy","Items you save up to buy","Footwear","Something you keep hidden","Items in a suitcase","Things with tails","Sports equipment","Crimes"
-            ]
-        },
-        {
-            id: 7,
-            name: 'List 8',
-            items: [
-                "Things that are sticky","Awards/ceremonies","Cars","Spices/Herbs","Bad habits","Cosmetics/Toiletries","Celebrities","Cooking utensils","Reptiles/Amphibians","Parks","Leisure activities","Things you're allergic to"
-            ]
-        },
-        {
-            id: 8,
-            name: 'List 9',
-            items: [
-                "Restaurants","Notorious people","Fruits","Things in a medicine cabinet","Toys","Household chores","Bodies of water","Authors","Halloween costumes","Weapons","Things that are round","Words associated with exercise"
-            ]
-        },
-        {
-            id: 9,
-            name: 'List 10',
-            items: [
-                "Sports","Song titles","Parts of the body","Ethnic foods","Things you shout","Birds","A girl's name","Ways to get from here to there","Items in a kitchen","Villains","Flowers","Things you replace"
-            ]
-        }
+            title: 'Beginner\'s pack',
+            description: 'This pack comes free with the app!',
+            lists: [
+            {
+                id: 0,
+                name: 'List 1',
+                items: [
+                    'A boy\'s name', 'Toys', 'An animal', 'Things that are cold', 'Insects', 'TV Shows', 'Things that grow', 'Fruits', 'Things that are black', 'School subjects', 'Movie titles', 'Musical Instruments'
+                ]
+            },
+            {
+                id: 1,
+                name: 'List 2',
+                items: [
+                    'Authors','Bodies of water','A bird','Countries','Cartoon characters','Holidays','Things that are square','European city','Clothing','A relative','Games','Sports Stars'
+                ]
+            },
+            {
+                id: 2,
+                name: 'List 3',
+                items: [
+                    'School supplies','Things that are hot','Heroes','A girl\'s name','Fears','TV Stars','Colors','A fish','Fruits','Provinces or States','Sports equipment','Tools'
+                ]
+            },
+            {
+                id: 3,
+                name: 'List 4',
+                items: [
+                    'Breakfast foods','Gifts','Flowers','Ice cream flavors','A drink','A river','Cities','Things in the kitchen','Ocean things','Nicknames','Hobbies','Parts of the body'
+                ]
+            },
+            {
+                id: 4,
+                name: 'List 5',
+                items: [
+                    'Sandwiches','Items in a catalog','World leaders/ Politicians','School subjects','Excuses for being late','Ice cream flavors','Things that jump/bounce','Television stars','Things in a park','Foreign cities','Stones/Gems','Musical instruments'
+                ]
+            },
+            {
+                id: 5,
+                name: 'List 6',
+                items: [
+                    'Nicknames','Things in the sky','Pizza toppings','Colleges/Universities','Fish','Countries','Things that have spots','Historical Figures','Something you\'re afraid of','Terms of endearment','Items in this room','Drugs that are abused'
+                ]
+            },
+            {
+                id: 6,
+                name: 'List 7',
+                items: [
+                    'Fictional characters','Menu items','Magazines','Capitals','Kinds of candy','Items you save up to buy','Footwear','Something you keep hidden','Items in a suitcase','Things with tails','Sports equipment','Crimes'
+                ]
+            },
+            {
+                id: 7,
+                name: 'List 8',
+                items: [
+                    'Things that are sticky','Awards/ Ceremonies','Cars','Spices/Herbs','Bad habits','Cosmetics/ Toiletries','Celebrities','Cooking utensils','Reptiles/Amphibians','Parks','Leisure activities','Things you\'re allergic to'
+                ]
+            },
+            {
+                id: 8,
+                name: 'List 9',
+                items: [
+                    'Restaurants','Notorious people','Fruits','Things in a medicine cabinet','Toys','Household chores','Bodies of water','Authors','Halloween costumes','Weapons','Things that are round','Words associated with exercise'
+                ]
+            },
+            {
+                id: 9,
+                name: 'List 10',
+                items: [
+                    'Sports','Song titles','Parts of the body','Ethnic foods','Things you shout','Birds','A girl\'s name','Ways to get from here to there','Items in a kitchen','Villains','Flowers','Things you replace'
+                ]
+            }
+        ]}
     ];
 
     S.Lists = {
@@ -93,27 +87,39 @@ var WordChallenge = window.WordChallenge || {};
             
             _.bindAll(this, 'onListSelected_');
 
-            var select = '<select id="list-selector"><option value="-1">-- Select list --</option>';
-
-            for (var i=0; i<lists.length; i++) {
-                var currentList = lists[i];
+            var select = '<select id=\'list-selector\'>';
+            for (var i=0; i < ListCollections[0].lists.length; i++) {
+                var currentList = ListCollections[0].lists[i];
                 select += '<option value="' + currentList.id + '">' + currentList.name + '</option>';
             }
             select += '</select>';
 
             el.html(select);
-            this.element_ = $("#list-selector");
+            this.element_ = $('#list-selector');
             this.element_.on('change', this.onListSelected_);
+            this.onOptionSelected_($(this.element_.find('option')[0]));
         },
 
         onListSelected_: function(e) {
             var selectedOption = $(e.target.options[e.target.selectedIndex]);
-            var selectedList = _.where(lists, { id: parseInt(selectedOption.val()) });
+            this.onOptionSelected_(selectedOption);
+        },
+
+        onOptionSelected_: function(selectedOption) {
+
+            var selectedList = _.where(ListCollections[0].lists, {
+                id: parseInt(selectedOption.val())
+            });
+
             var orderedList = '<ol><li>';
-            orderedList += selectedList[0].items.join('</li><li>'); 
-            orderedList += "</li></ol>";
+            orderedList += selectedList[0].items.join('</li><li>');
+            orderedList += '</li></ol>';
             $('.list').html(orderedList);
-            window.myScroll.refresh();
+
+            if (window.myScroll) {
+                window.myScroll.refresh();
+            }
+
         }
 
     };
