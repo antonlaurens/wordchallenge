13655c6b33a3ba3f20b532fad83422a371942a62
Anton Laurens <antonlaurens@gmail.com>
2014-02-23 18:19:22 +0000

    1. updated lists 2. hide pack selector + help button

--- js/lists.js
+++ js/lists.js
@@ -10,70 +10,112 @@ var WordChallenge = window.WordChallenge || {};
                 id: 0,
                 name: 'List 1',
                 items: [
-                    'A boy\'s name', 'Toys', 'An animal', 'Things that are cold', 'Insects', 'TV Shows', 'Things that grow', 'Fruits', 'Things that are black', 'School subjects', 'Movie titles', 'Musical Instruments'
+                    'A boy\'s name', 'Cities', 'Things that are cold', 'Things you don\'t want in the house', 'Sports teams', 'Insects', 'Things in a coffee bar', 'Things you mix up', 'TV shows', 'Things found in the ocean', 'Kinds of weather', 'Foods that kids hate'
                 ]
             },
             {
                 id: 1,
                 name: 'List 2',
                 items: [
-                    'Authors','Bodies of water','A bird','Countries','Cartoon characters','Holidays','Things that are square','European city','Clothing','A relative','Games','Sports Stars'
+                    'Famous females', 'Medicines/ drugs', 'Machines', 'Hobbies', 'Things you do in the morning', 'Things you plug in', 'Animals', 'Languages', 'Things you grab on your way out the door', 'Junk foods', 'Things that grow', 'Things in an arcade'
                 ]
             },
             {
                 id: 2,
                 name: 'List 3',
                 items: [
-                    'School supplies','Things that are hot','Heroes','A girl\'s name','Fears','TV Stars','Colors','A fish','Fruits','Provinces or States','Sports equipment','Tools'
+                    'Articles of clothing', 'Desserts', 'Car parts', 'Shoes', 'Athletes', '4-letter words', 'Things you fold', 'Things in a bedroom', 'Things you do online', 'Things at the beach', 'Things you dream about', 'Tools'
                 ]
             },
             {
                 id: 3,
                 name: 'List 4',
                 items: [
-                    'Breakfast foods','Gifts','Flowers','Ice cream flavors','A drink','A river','Cities','Things in the kitchen','Ocean things','Nicknames','Hobbies','Parts of the body'
+                    'Heroes', 'Gifts', 'Terms of endearment', 'Kinds of dances', 'Things that are black', 'Vehicles', 'Things found in an arena', 'Things people gossip about', 'Weekend activities', 'Things in a souvenir shop', 'Items in your purse/ wallet', 'World records'
                 ]
             },
             {
                 id: 4,
                 name: 'List 5',
                 items: [
-                    'Sandwiches','Items in a catalog','World leaders/ Politicians','School subjects','Excuses for being late','Ice cream flavors','Things that jump/bounce','Television stars','Things in a park','Foreign cities','Stones/Gems','Musical instruments'
+                    'Sandwiches', 'Things you can do with your feet', 'World leaders/ politicians', 'School subjects', 'Excuses for being late', 'Types of ice cream', 'Things with balls', 'Television subjects', 'Things in a park', 'Countries', 'Stones/ gems', 'Musical instruments'
                 ]
             },
             {
                 id: 5,
                 name: 'List 6',
                 items: [
-                    'Nicknames','Things in the sky','Pizza toppings','Colleges/Universities','Fish','Countries','Things that have spots','Historical Figures','Something you\'re afraid of','Terms of endearment','Items in this room','Drugs that are abused'
+                    'Nicknames', 'Things in the sky', 'Things with windows', 'Universities', 'Fish', 'Things you add water to', 'Things that have spots', 'Things that smell bad', 'Things you\'re afraid of', 'Terms of measurement', 'Items in this room', 'Book titles'
                 ]
             },
             {
                 id: 6,
                 name: 'List 7',
                 items: [
-                    'Fictional characters','Menu items','Magazines','Capitals','Kinds of candy','Items you save up to buy','Footwear','Something you keep hidden','Items in a suitcase','Things with tails','Sports equipment','Crimes'
+                    'Fictional characters', 'Places to go on a date', 'Magazines', 'Famous landmarks', 'Sweet things', 'Things you save up to buy', 'Footwear', 'Something you keep hidden', 'Items in a suitcase', 'Things with tails', 'Sports equipment', 'Crimes'
                 ]
             },
             {
                 id: 7,
                 name: 'List 8',
                 items: [
-                    'Things that are sticky','Awards/ Ceremonies','Cars','Spices/Herbs','Bad habits','Cosmetics/ Toiletries','Celebrities','Cooking utensils','Reptiles/Amphibians','Parks','Leisure activities','Things you\'re allergic to'
+                    'Things that are sticky', 'Awards/ ceremonies', 'Cars', 'Spices/ herbs', 'Bad habits', 'Cosmetics/ toiletries', 'Celebrities', 'Thing to do with leftover turkey', 'Reptiles/ amphibians', 'Fads', 'Leisure acitivities', 'Things you\'re allergic to'
                 ]
             },
             {
                 id: 8,
                 name: 'List 9',
                 items: [
-                    'Restaurants','Notorious people','Fruits','Things in a medicine cabinet','Toys','Household chores','Bodies of water','Authors','Halloween costumes','Weapons','Things that are round','Words associated with exercise'
+                    'Restaurants', 'Notorious people', 'Fruits', 'Weapons', 'Toys', 'Household chores', 'Bodies of water', 'Authors', 'Halloween costumes', 'Funny movies', 'Things that are round', 'Things in pet stores'
                 ]
             },
             {
                 id: 9,
                 name: 'List 10',
                 items: [
-                    'Sports','Song titles','Parts of the body','Ethnic foods','Things you shout','Birds','A girl\'s name','Ways to get from here to there','Items in a kitchen','Villains','Flowers','Things you replace'
+                    'Types of sports', 'Song titles', 'Parts of the body', 'Spicy foods', 'Things you shout', 'Birds', 'A girl\'s name', 'Ways to get from here to there', 'Cooking ingredients', 'Villains/ monsters', 'Flowers', 'Things that weigh less than one pound'
+                ]
+            },
+            {
+                id: 10,
+                name: 'List 11',
+                items: [
+                    'Things that need to be cleaned', 'Famous duos and trios', 'Things found in a desk', 'Island hot spots', 'Things at a hospital', 'Words associated with money', 'Items in a vending machine', 'Movie titles', 'Games', 'Things that you wear', 'Beers', 'Things at a circus'
+                ]
+            },
+            {
+                id: 11,
+                name: 'List 12',
+                items: [
+                    'Vegetables', 'Things that make you scream', 'Things you throw away', 'Careers', 'Things you celebrate', 'Cartoon characters', 'Types of drinks', 'Musical groups', 'Science things', 'Things to do on a rainy day', 'Trees', 'Personality traits'
+                ]
+            },
+            {
+                id: 12,
+                name: 'List 13',
+                items: [
+                    'Things you can tie in a knot', 'Things that are soft', 'Thing in a sci-fi movie', 'Word with four different vowels', 'Things that kids play with', 'Things at a wedding', 'Hot places', 'Things in outer space', 'Sounds', 'Things that are cute', 'Famous singers', 'Things at an amusement park'
+                ]
+            },
+            {
+                id: 13,
+                name: 'List 14',
+                items: [
+                    'Things that are worn above the waist', 'Things that are bright', 'Things that have numbers', 'Things found in a gym/ health club', 'Chain stores', 'Ways to say hi and bye', 'Things in a garden', 'Things people use to decorate their houses', 'Items in an office', 'Things in pairs or sets', 'Artists', 'Cruise ship destinations'
+                ]
+            },
+            {
+                id: 14,
+                name: 'List 15',
+                items: [
+                    'Things at a zoo', 'Things smaller than your fist', 'Things that fly', 'Things you eat on a diet', 'Worlds ending in "ly"', 'Things found under the table', 'Things in a hotel', 'Things you can do in less that 5 minutes', 'Things with claws', 'Party things', 'Reasons to skip school/ work', 'Titles people have'
+                ]
+            },
+            {
+                id: 15,
+                name: 'List 16',
+                items: [
+                    'Things that go well with chocolate', 'Things in a mystery novel', 'Websites', 'Loud things', 'Things you eat with a spoon', 'Famous sayings', 'Underground things', 'Things that are wet', 'Things in an airport', 'Words with double letters', 'Things a baby uses', 'Things in fairy tales'
                 ]
             }
         ]}
