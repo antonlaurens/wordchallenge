81564dd8968901643f8afd7c391e90330414007e
Anton Laurens <anton.laurens@corp.badoo.com>
2014-04-06 21:41:09 +0100

    refactor init to do a startup

--- js/init.js
+++ js/init.js
@@ -1,60 +1,92 @@
 var WordChallenge = window.WordChallenge || {};
-(function(S) {
 
-    if (window.navigator.standalone) {
-        $("meta[name='apple-mobile-web-app-status-bar-style']").remove();
-        $('#timer').addClass('extra-margin');
-    }
+(function (WC) {
 
-    if (S.Timer) {
-        S.Timer.init({
-            timer: $("#timer"),
-            startButton: $("#start-button"),
-            resetButton: $("#reset-button"),
-            letterContainer: $("#letter")
-        });
-    }
+    var listScroller = null;
 
-    if (S.Lists) {
-        S.Lists.init($("#list-select-placeholder"));
-    }
+    WC.Init = {
 
-    //prevent scrolling
-    document.body.addEventListener('touchmove', function(e){ e.preventDefault(); });
+        init: function () {
 
-    window.addEventListener('load', function() {
-        FastClick.attach(document.getElementsByClassName('buttons-container')[0]);
+            _.bindAll(this, 'onStartup_');
+
+            listScroller = new iScroll('wrapper');
+
+            WC.Timer.init({
+                timer: $("#timer"),
+                startButton: $("#start-button"),
+                resetButton: $("#reset-button"),
+                letterContainer: $("#letter")
+            });
+
+            WC.Lists.init($("#list-select-placeholder"));
+
+            FastClick.attach(document.getElementsByClassName('buttons-container')[0]);
+
+            this.resizeScroller();
+
+            this.doStartup_();
+        },
+
+        doStartup_: function () {
+            var uid = window.localStorage.getItem('uid');
+            if (!uid) {
+                uid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                    var r = Math.random() * 16 | 0, v = c == 'x' ? r : r & 0x3 | 0x8;
+                    return v.toString(16);
+                });
+                window.localStorage.setItem('uid', uid);
+            }
+            $.post('/startup', { uid: uid }, this.onStartup_);
+        },
+
+        onStartup_: function (response) {
+            console.log(response);
+        },
+
+        resizeScroller: function () {
+
+            if (!listScroller) {
+                return;
+            }
+
+            var topHeight = $('.top').height();
+            var scrollerHeight = window.innerHeight - topHeight;
+
+            $('#wrapper').css({
+                position: 'fixed',
+                top: topHeight + 10 + 'px',
+                left: '0px',
+                height: scrollerHeight - 10 + 'px',
+                width: '100%'
+            });
+
+            listScroller.refresh();
+
+        }
+
+    };
+
+
+    window.addEventListener('load', function () {
+
     }, false);
 
-    function resizeScroller() {
-        var scrollerHeight = window.innerHeight - $('.top').height();
-        $('#wrapper').css({
-            position: 'fixed',
-            top: $('.top').height() + 10 + 'px',
-            left: '0px',
-            height: scrollerHeight - 10 + 'px',
-            width: '100%'
-        });
-        window.myScroll.refresh();
-    }
+    // event listeners
+    window.addEventListener('resize', WC.Init.resizeScroller);
+    document.addEventListener('DOMContentLoaded', WC.Init.init(), false);
+    document.body.addEventListener('touchmove', function (e) {
+        e.preventDefault();
+    });
 
-    function loaded() {
-        window.myScroll = new iScroll('wrapper');
-        resizeScroller();
-    }
+    window.requestAnimFrame = (function () {
 
-    window.addEventListener('resize', resizeScroller);
-    document.addEventListener('DOMContentLoaded', loaded, false);
+        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
 
-    window.requestAnimFrame = (function(){
-        return window.requestAnimationFrame       || 
-          window.webkitRequestAnimationFrame || 
-          window.mozRequestAnimationFrame    || 
-          window.oRequestAnimationFrame      || 
-          window.msRequestAnimationFrame     || 
-          function( callback ){
-            window.setTimeout(callback, 1000 / 60);
-          };
-})();
+            function (callback) {
+                window.setTimeout(callback, 1000 / 60);
+            };
+
+    })();
 
 })(WordChallenge);
